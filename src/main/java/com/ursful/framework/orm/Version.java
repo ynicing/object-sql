716748cdeda90015f416d7b939187eec501d1cc6
ynicing <jlis@qq.com>
2018-09-12 08:34:35 +0800

    1.3.2

--- src/main/java/com/ursful/framework/orm/Version.java
+++ src/main/java/com/ursful/framework/orm/Version.java
@@ -4,5 +4,5 @@ package com.ursful.framework.orm;
  * Created by Administrator on 2018/7/11.
  */
 public class Version {
-    public static final String version = "1.3.1";
+    public static final String version = "1.3.2";
 }
